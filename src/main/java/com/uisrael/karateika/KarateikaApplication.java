1c9d18756000579b08a0c0e2d6fffa75da45712a
Xavier Condor <ftecdevelopment@gmail.com>
2025-12-17 21:05:40 -0500

    Proyecto iniciado

---
+++ src/main/java/com/uisrael/karateika/KarateikaApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.karateika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarateikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uisrael/karateika/KarateikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.karateika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarateikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
